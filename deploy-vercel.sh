2a76ddbbfa0e7d1265b86adbd0b4b9073fa2dcae
naseer2426 <naseerfathima2426@gmail.com>
2024-12-26 19:22:52 +0400

    fixed deploy-vercel.sh

--- deploy-vercel.sh
+++ deploy-vercel.sh
@@ -3,8 +3,5 @@
 # Install deno
 curl -fsSL https://deno.land/x/install/install.sh | sh
 
-# Install pagic
-/vercel/.deno/bin/deno install --unstable --allow-read --allow-write --allow-net --allow-import --global https://deno.land/x/pagic/mod.ts
-
-# Pagic build
-/vercel/.deno/bin/deno run --unstable --allow-read --allow-write --allow-net --allow-env --allow-run --allow-import https://deno.land/x/pagic/mod.ts build
+# Deno Build
+/vercel/.deno/bin/deno build
