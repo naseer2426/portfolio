430924528b81583a5d04513a1210ac20a9c09dba
naseer2426 <naseerfathima2426@gmail.com>
2024-12-27 09:50:24 +0400

    added tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-import './App.css'
-// @ts-expect-error Unable to infer type at the moment
-import reactLogo from './assets/react.svg'
+import { LandingSection } from "./sections/landing.tsx";
 
 function App() {
   return (
     <>
-      <img src="/vite-deno.svg" alt="Vite with Deno" />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Work in progress!</h1>
+      <LandingSection />
     </>
   )
 }
---
+++ src/sections/landing.tsx
@@ -0,0 +1,14 @@
+// @deno-types="@types/react"
+import {FC} from 'react';
+
+const LandingSection: FC = () => {
+    return (
+        <div className="text-3xl font-bold underline">
+            Hello
+        </div>
+    )
+}
+
+export {
+    LandingSection
+}
