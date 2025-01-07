1e791c327ad6ba65ae60dbf30e7959b54cfa697a
naseer2426 <naseerfathima2426@gmail.com>
2025-01-07 17:21:46 +0400

    responsive improvements

--- src/sections/intro/index.tsx
+++ src/sections/intro/index.tsx
@@ -18,40 +18,11 @@ const IntroSection: FC = () => {
         <>
             <div className="h-svh flex flex-col">
                 <Header />
-                <div className='flex flex-col px-8 mt-8 h-full gap-12 items-center md:mt-0 md:justify-between md:flex-row md:px-16 md:gap-8'>
+                <div className='flex flex-col px-8 mt-8 h-full gap-12 items-center md:mt-0 md:justify-between md:flex-row md:px-16'>
                     <div className='flex gap-8 flex-col max-w-md'>
                         <div className='flex gap-4 flex-col'>
-                            <Carousel
-                                opts={{
-                                    align: "start",
-                                    loop: true,
-                                }}
-                                plugins={[
-                                    Autoplay({
-                                        delay: 1500,
-                                    }),
-                                ]}
-                                orientation='vertical'
-                            >
-                                <CarouselContent className='max-h-[3rem]'>
-                                    <CarouselItem>
-                                        <h2 className='text-accent role'>Backend Engineer</h2>
-                                    </CarouselItem>
-                                    <CarouselItem>
-                                        <h2 className='text-accent role'>Frontend Engineer</h2>
-                                    </CarouselItem>
-                                    <CarouselItem>
-                                        <h2 className='text-accent role'>Rubik's Cube Enthusiast</h2>
-                                    </CarouselItem>
-                                    <CarouselItem>
-                                        <h2 className='text-accent role'>Physics Lover</h2>
-                                    </CarouselItem>
-                                    <CarouselItem>
-                                        <h2 className='text-accent role'>Singer</h2>
-                                    </CarouselItem>
-                                </CarouselContent>
-                            </Carousel>
-                            <h1 className='text-4xl md:text-6xl font-extrabold font-greeting name'>Hey! My name is<br />Naseer Ahmed Khan</h1>
+                            <SkillCaraoursel />
+                            <h1 className='text-4xl md:text-5xl lg:text-6xl lg:min-w-max font-extrabold font-greeting name'>Hey! My name is<br />Naseer Ahmed Khan</h1>
                         </div>
                         <p>Hi I am noob software developer trying to learn new things. To be updated using ChatGPT in the future</p>
                         <div className='flex flex-row gap-4'>
@@ -59,13 +30,48 @@ const IntroSection: FC = () => {
                             <Button variant='outline'>Projects</Button>
                         </div>
                     </div>
-                    <div>
-                        <img src={me} className='h-[24rem] me'></img>
+                    <div className='size-56 md:size-64 lg:size-72'>
+                        <img src={me} className='me'></img>
                     </div>
                 </div>
             </div>
         </>
 
+    )
+}
+
+const SkillCaraoursel: FC = () => {
+    return (
+        <Carousel
+            opts={{
+                align: "start",
+                loop: true,
+            }}
+            plugins={[
+                Autoplay({
+                    delay: 1500,
+                }),
+            ]}
+            orientation='vertical'
+        >
+            <CarouselContent className='max-h-[3rem]'>
+                <CarouselItem>
+                    <h2 className='text-accent role'>Backend Engineer</h2>
+                </CarouselItem>
+                <CarouselItem>
+                    <h2 className='text-accent role'>Frontend Engineer</h2>
+                </CarouselItem>
+                <CarouselItem>
+                    <h2 className='text-accent role'>Rubik's Cube Enthusiast</h2>
+                </CarouselItem>
+                <CarouselItem>
+                    <h2 className='text-accent role'>Physics Lover</h2>
+                </CarouselItem>
+                <CarouselItem>
+                    <h2 className='text-accent role'>Singer</h2>
+                </CarouselItem>
+            </CarouselContent>
+        </Carousel>
     )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host:'0.0.0.0',
+  },
   plugins: [react()],
   resolve: {
     alias: {
