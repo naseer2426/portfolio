cf9ea67c3645b7909943ac2515faf29d78d594fc
naseer2426 <naseerfathima2426@gmail.com>
2025-10-27 00:41:09 +0800

    added speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Nunito } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
 
 const playfair = Playfair_Display({
@@ -34,6 +35,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
