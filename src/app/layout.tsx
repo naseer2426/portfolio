9120f17d164dee0b478ad6e33902031fac98f3a4
naseer2426 <naseerfathima2426@gmail.com>
2025-10-27 00:33:01 +0800

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Nunito } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const playfair = Playfair_Display({
@@ -32,6 +33,7 @@ export default function RootLayout({
         className={`${playfair.variable} ${nunito.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
